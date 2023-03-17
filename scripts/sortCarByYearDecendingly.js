f37481e1bd7a74235a8d97061afea7dad73e67a0
MHelmiAzhar <helmiazhar311201@gmail.com>
2023-03-17 21:30:42 +0700

    first commit

---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,31 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  let result = [...cars];
+
+  // Tulis code-mu disini
+  function bblSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      // Last i elements are already in place
+      for (var j = 0; j < arr.length - i - 1; j++) {
+        // Checking if the item at present iteration
+        // is greater than the next iteration
+        if (arr[j].year < arr[j + 1].year) {
+          // If the condition is true then swap them
+          var temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+
+  result = bblSort(result);
+  console.log(result, "ini hasil");
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
